38b5566a41216bd2d1a092cbfcce686ce03b7ee3
Jeremy Lee <jlee00@stuy.edu>
2019-04-08 15:17:56 -0400

    Wrote apply

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -113,7 +113,7 @@ public class ExpressionTree{
 
   /*use the correct operator on both a and b, and return that value*/
   private double apply(char op, double a, double b){
-    /*you are to write this method*/
-    return 0.0;
+    String input = op + " " + a + " " + b;
+    return Calculator.eval(input);
   }
 }
